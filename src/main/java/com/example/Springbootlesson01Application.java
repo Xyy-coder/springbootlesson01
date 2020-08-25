9532b56cb7c44d019ff1a252f305bbffbba91288
repo_glb <repo@qq.com>
2020-08-25 21:27:57 +0800

    Initial commit

---
+++ src/main/java/com/example/Springbootlesson01Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootlesson01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springbootlesson01Application.class, args);
+    }
+
+}
